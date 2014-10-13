04b3b450fb8c3ce47edf213981e12cdd89577a81
Yanke <565171252@qq.com>
2014-10-13 16:23:53 +0800

    add lab2

---
+++ lab2/src/lab2/BookDelete.java
@@ -0,0 +1,31 @@
+package lab2;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class BookDelete extends ActionSupport{
+	
+	private static final long serialVersionUID = 1L;
+	private String bookname;
+	
+	public  String execute() throws Exception{
+		List<String[]> mylist= new ArrayList<String[]>();
+		
+		bookname = getBookname();
+		String message = DBConnection.delete("dictionary","name",bookname);
+		mylist = DBConnection.query("name",bookname,"dictionary");
+		String message2 = DBConnection.delete("author","Name",bookname);
+		if(message=="success")return "success";
+		else return "failed";
+	}
+	
+	
+	
+	public String getBookname() {
+		return bookname;
+	}
+	public void setBookname(String bookname) {
+		this.bookname = bookname;
+	}
+}
